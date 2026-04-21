27c876f02c60d7f2968f472ca30c6da254e0037b
Gabriel Infante <gabriel.infante@uptc.edu.co>
2026-04-21 09:50:39 -0500

    Ajustes a tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,48 @@
 import type { Config } from 'jest';
 
+const esModules = [
+  '@angular',
+  'tslib',
+  'jest-preset-angular',
+  '@angular/platform-browser-dynamic',
+].join('|');
+
 const config: Config = {
-  preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  globalSetup: 'jest-preset-angular/global-setup',
   testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|css|scss|svg)$',
+      useESM: true,
+    },
+  },
+  transform: {
+    '^.+\\.(ts|html|mjs)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'mjs', 'js', 'html', 'json'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^rxjs$': '<rootDir>/node_modules/rxjs/dist/cjs/index.js',
+    '^rxjs/operators$': '<rootDir>/node_modules/rxjs/dist/cjs/operators/index.js',
+    '^rxjs/(.*)$': '<rootDir>/node_modules/rxjs/dist/cjs/$1',
+  },
+  transformIgnorePatterns: [`node_modules/(?!(${esModules})/)`],
+  collectCoverage: true,
+  coverageReporters: ['lcov', 'text', 'clover'],
+  coverageDirectory: 'coverage',
+  reporters: [
+    'default',
+    [
+      'jest-sonar',
+      {
+        outputDirectory: 'reports',
+        outputName: 'test-report.xml',
+        reportedFilePath: 'relative',
+      },
+    ],
+  ],
 };
 
 export default config;
--- setup-jest.ts
+++ setup-jest.ts
@@ -1,3 +1,18 @@
-import {setupZoneTestEnv} from "jest-preset-angular/setup-env/zone";
+import { setupZonelessTestEnv } from 'jest-preset-angular/setup-env/zoneless';
+import { ɵresolveComponentResources as resolveComponentResources } from '@angular/core';
 
-setupZoneTestEnv();
+setupZonelessTestEnv({
+  errorOnUnknownElements: false,
+  errorOnUnknownProperties: false,
+});
+
+// Mock fetch nativo sin jest.fn()
+(globalThis as any).fetch = (_url: string) =>
+  Promise.resolve({
+    text: () => Promise.resolve(''),
+    ok: true,
+  } as Response);
+
+beforeEach(async () => {
+  await resolveComponentResources(fetch);
+});
